1d66be76941c22cb52e8397d8f52f23a907ba990
lildibbb <adib.asyraaf760@gmail.com>
2024-11-12 12:49:08 +0800

    create sendEmail function

---
+++ server/src/services/sendEmail.ts
@@ -0,0 +1,30 @@
+// server/services/sendEmail.ts
+import * as nodemailer from "nodemailer";
+import type { Email } from "../../types";
+
+export async function sendEmail(reqBody: Email) {
+  const { to, subject, html } = reqBody;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Failed to send email");
+  }
+}
