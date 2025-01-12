fa5a3ab3066ed9bc44211b1ec5e8f5341d5d96f5
lildibbb <adib.asyraaf760@gmail.com>
2025-01-12 14:22:46 +0800

    turn off devtool

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -13,7 +13,7 @@ export const Route = createRootRoute({
   component: () => (
     <div className="min-h-screen  ">
       <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
+      {/* <TanStackRouterDevtools position="bottom-right" /> */}
     </div>
   ),
 });
